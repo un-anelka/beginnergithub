bc0b1bc291d2ec6ef2e3a6a231466a950d648ef4
andela <andela@andelas-MacBook-Pro.local>
2022-03-23 17:50:22 +0200

    Test one added and removed item

--- Subscription/Routes_Subscription/Subscription_routes.js
+++ Subscription/Routes_Subscription/Subscription_routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-// import {subscriptionPost} from "../Controllers_Subscription/subscription_functions.js"
+
 import {subscriptionPost, subscriptionGetAll, subscriptionUpdate, subscriptionDelete, subscriptionGetOne} from "../Controllers_subscription/Subscription_functions.js"
 import { verifyToken } from '../../Usersfolder/middlewares_users/users_functions.js';
 import Subscription_validator from '../middlewares_contacts/Subscription_validator.js';
